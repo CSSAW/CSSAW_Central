1f571bf51ed04aa0fa5b96645f0a67cd9e046ee7
CSLukeW <williams.luke.2001@gmail.com>
2020-06-17 22:55:02 -0600

    __main__ sql execution script

--- query.py
+++ query.py
@@ -8,26 +8,27 @@ in the python shell)
 
 """
 
-import os
 import mysql.connector
 from mysql import OperationalError
 import argparse
 import logging
+import json
 
 def connect(user, password, host):
-    """ connects to database and returns cursor """
+    """ connects to database and returns dict cursor """
 
     try:
         cnct = mysql.connector.connect(
             user=user,
             password=password,
-            host=host
+            host=host,
+            database='CENTRAL'
         )
-        return cnct.cursor()
+        return cnct.cursor(dictionary=True)
     except mysql.connector.Error as err:
         logging.error("Connection error: ", err)
 
-def executeSQL(filename, cursor):
+def execute_SQL(filename, cursor):
     """ execute .SQL file of commands """
 
     # open file
@@ -44,3 +45,30 @@ def executeSQL(filename, cursor):
             cursor.execute(command)
         except mysql.connector.OperationalError:
             logging.error('Operation \"' + command '\" failed, skipping...')
+
+def get_JSON_from_cursor(cursor):
+    """ Return JSON string from cursor results """
+
+    outS = ''
+    for result in cursor:
+        outS += json.dumps(result, JSONout, indent=4)
+
+    return outS
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host', help='Host IP for server')
+    parser.add_argument('user', help='Database account for login')
+    parser.add_argument('password', default=None, help='Password for database user')
+    parser.add_argument('querypath', help='Path of query to execute')
+    parser.add_argument('-outfile', default='./results/result.json', help='Filename of output json file')
+    args = parser.parse_args()
+
+    # connect to server using given user and pass
+    cursor = connect(args.user, args.password, args.host)
+
+    # execute external sql file
+    execute_SQL(args.querypath, cursor)
+
+    with open(args.outfile, 'w+') as outfile:
+        outfile.write(get_JSON_from_cursor(cursor))

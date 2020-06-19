69053287abe25dcefd920c7f63c9f258f89d8b49
CSLukeW <williams.luke.2001@gmail.com>
2020-06-18 21:36:33 -0600

    Connect and file execution tests

--- test.py
+++ test.py
@@ -2,21 +2,32 @@ from query import *
 import datetime
 
 def test_connect():
-    crsr = connect('test', 'test', open('host.txt').read())
-    assert crsr != None
-    assert len(crsr.column_names == 2) & crsr.column_names[0] == u'column1' & crsr.column_names[1] == u'column2'
+    host = open('host.txt')
+    sess = Session('test', 'test', host.read())
+    host.close()
+
+    op = "SELECT * FROM test_table;"
+    sess.cursor.execute(op)
+    for row in sess.cursor:
+        print(row['column1'], row['column2'])
+        assert (row['column1'] == '6/18/2020') & (row['column2'] == 'test')
 
 def test_executeSQL():
-    crsr = connect('test', 'test', open('host.txt').read())
+    host = open('host.txt')
+    sess = Session('test', 'test', host.read())
+    host.close()
 
-    execute_SQL('./queries/test.sql', crsr)
-
-    buffer = []
-    for row in crsr:
-        buffer.append(row)
-
-    assert buffer[0]['column1'] + buffer[0]['column2'] == '6/18/2020test'
+    try:
+        sess.execute_SQL('./queries/test.sql')
+    except mysql.connector.errors.Error as err:
+        logging.error('Error: {}'.format(err))
 
 if __name__ == '__main__':
+    print('Test connect')
     test_connect()
+    print('Connect successful')
+    print('Execute SQL File Test')
     test_executeSQL()
+    print('Successful SQL File Execution')
+    print()
+    print('Test Completed')

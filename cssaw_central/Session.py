223f54c3cef19317128a3465c65fd19400020c01
Luke WIlliams <williams.luke.2001@gmail.com>
2020-06-21 20:08:53 -0600

    Documentation updates

--- cssaw_central/Session.py
+++ cssaw_central/Session.py
@@ -5,12 +5,26 @@ import logging
 
 class Session:
     def __init__(self, user, password, host, db='Test'):
+        """ Initialize connection to database
+
+            args:
+                user ---- name of user to connect as
+                password ---- password for specified user
+                host ---- host ip to connect to
+
+            kwargs:
+                db ---- database to connect to (defaults to test)
+        """
         self.engine = alc.create_engine("mysql+pymysql://{}:{}@{}/{}".format(user, password, host, db))
 
         self.conn = self.engine.connect()
 
     def execute_SQL(self, filename):
-        """ execute .SQL file of commands """
+        """ execute .SQL file of commands 
+        
+            args:
+                filename ---- path of file to execute (must be .sql)
+        """
 
         # open file
 
@@ -35,7 +49,15 @@ class Session:
         return results
 
     def insert(self, table, columns, rows):
-        """ insert given rows into given table """
+        """ insert given rows into given table 
+        
+            args:
+                table ---- name of table to insert into
+                columns ---- list of column names
+                rows ---- list of values to put into corresponding columns
+
+                len(columns) MUST EQUAL len(rows)
+        """
 
         # query type
         query = alc.insert(table)
@@ -48,7 +70,14 @@ class Session:
         # execute and commit query
         self.conn.execute(query, values_list)
 
-    def insert_from_CSV(self, filename, table, primary_key):
+    def insert_from_CSV(self, filename, table):
+        """ Inserts entire CSV file into specified table
+
+            args:
+                filename ---- file path of data to upload
+                table ---- name of table to insert into
+
+        """
         with open(filename, 'r') as f:
 
             # pull headers

9ed488da0839daf18696329d6b12c07c00846c5c
CSLukeW <williams.luke.2001@gmail.com>
2020-06-26 12:24:26 -0600

    Add overwrite argument

--- cssaw_central/csvsql.py
+++ cssaw_central/csvsql.py
@@ -10,10 +10,11 @@ def main():
     parser.add_argument('database', help='database to run queries on')
     parser.add_argument('table', help='table to insert into (will create new if doesn\'t exist)')
     parser.add_argument('filename', help='filepath of CSV file to insert')
+    parser.add_argument('--overwrite', action='store_true', 'help='Overwrite already-present table in database')
     args = parser.parse_args()
 
     # create session for login
     sess = Session.Session(args.user, args.password, args.host, db=args.database)
 
     # use insert_from_CSV() method to insert csv into table
-    sess.insert_from_CSV(args.filename, args.table)
+    sess.insert_from_CSV(args.filename, args.table, args.overwrite)

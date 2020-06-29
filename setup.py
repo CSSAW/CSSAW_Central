afe3755b6cb7229221c7cd542fbc0e7f6d53c980
CSLukeW <williams.luke.2001@gmail.com>
2020-06-29 00:29:42 -0600

    Release v1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cssaw_central",
-    version="1.2.0",
+    version="1.2.1",
     author="Luke Williams",
     author_email="williams.luke.2001@gmail.com",
     description="Access Point Module to CSSAW Database",
